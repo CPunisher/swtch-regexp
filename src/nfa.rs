ea096e4ed5662fc93016793464ac4af8002485d3
CPunisher <1343316114@qq.com>
2023-10-13 00:23:46 +0800

    feat: merge graph

--- src/nfa.rs
+++ src/nfa.rs
@@ -4,6 +4,37 @@ type State = NodeIndex<u32>;
 type NodeLabel = String;
 type EdgeLabel = Option<char>;
 
+fn merge_graph<N: Clone, E: Clone>(g1: &Graph<N, E>, g2: &Graph<N, E>) -> Graph<N, E> {
+    let mut graph = Graph::new();
+    let node_mapping_1: Vec<_> = g1
+        .node_indices()
+        .map(|node_index| {
+            let node_weight = g1.node_weight(node_index).unwrap().clone();
+            graph.add_node(node_weight.clone())
+        })
+        .collect();
+    let node_mapping_2: Vec<_> = g2
+        .node_indices()
+        .map(|node_index| {
+            let node_weight = g2.node_weight(node_index).unwrap().clone();
+            graph.add_node(node_weight.clone())
+        })
+        .collect();
+
+    for edge in g1.raw_edges() {
+        let source = node_mapping_1[edge.source().index()];
+        let target = node_mapping_1[edge.target().index()];
+        graph.add_edge(source, target, edge.weight.clone());
+    }
+
+    for edge in g2.raw_edges() {
+        let source = node_mapping_2[edge.source().index()];
+        let target = node_mapping_2[edge.target().index()];
+        graph.add_edge(source, target, edge.weight.clone());
+    }
+    graph
+}
+
 pub struct Nfa {
     graph: Graph<NodeLabel, EdgeLabel>,
     initial_state: State,
@@ -19,25 +50,6 @@ impl Nfa {
 }
 
 impl Nfa {
-    fn merge(nfa_1: &Nfa, nfa_2: &Nfa) -> Self {
-        let mut nfa = Self::default();
-        nfa.graph.extend_with_edges(
-            nfa_1
-                .graph
-                .raw_edges()
-                .iter()
-                .map(|edge| (edge.source(), edge.target(), edge.weight)),
-        );
-        nfa.graph.extend_with_edges(
-            nfa_2
-                .graph
-                .raw_edges()
-                .iter()
-                .map(|edge| (edge.source(), edge.target(), edge.weight)),
-        );
-        nfa
-    }
-
     pub fn literal_character(c: char) -> Self {
         let mut nfa = Self::default();
         let state = nfa.graph.add_node("".to_string());
@@ -45,15 +57,15 @@ impl Nfa {
         nfa
     }
 
-    pub fn catenation(self, rhs: Nfa) -> Self {
-        let mut nfa = Self::merge(&self, &rhs);
-        nfa.graph
-            .add_edge(nfa.initial_state, self.initial_state, None);
-        for sink in self.accepted_states() {
-            nfa.graph.add_edge(sink, rhs.initial_state, None);
-        }
-        nfa
-    }
+    // pub fn catenation(self, rhs: Nfa) -> Self {
+    //     let mut nfa = Self::merge(&self, &rhs);
+    //     nfa.graph
+    //         .add_edge(nfa.initial_state, self.initial_state, None);
+    //     for sink in self.accepted_states() {
+    //         nfa.graph.add_edge(sink, rhs.initial_state, None);
+    //     }
+    //     nfa
+    // }
 
     // pub fn alternation(self, rhs: Nfa) -> Self {}
 
@@ -99,11 +111,11 @@ mod tests {
 
     #[test]
     fn test_merge() {
-        let nfa_1 = Nfa::literal_character('a');
-        let nfa_2 = Nfa::literal_character('b');
-        let nfa = Nfa::merge(&nfa_1, &nfa_2);
-        assert_eq!(nfa.graph.node_count(), 4);
-        assert_eq!(nfa.graph.edge_count(), 2);
+        let g1 = Nfa::literal_character('a').graph;
+        let g2 = Nfa::literal_character('b').graph;
+        let graph = merge_graph(&g1, &g2);
+        assert_eq!(graph.node_count(), 4);
+        assert_eq!(graph.edge_count(), 2);
     }
 
     #[test]
